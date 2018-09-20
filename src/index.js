c94e83f26000b7322457eafd02bbc8a27b8963c6
panikutysh <pani.kutysh@gmail.com>
2018-09-20 16:58:21 +0300

    getZerosCount

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var CountZeros = 0;
+  Efficient5 = 0; 
+  Divider = number;
+  while (Divider>=5) 
+  {
+    Divider = Divider/5;
+    Efficient5 = Efficient5 + 1;
+  }
+  for (let index = 1; index < Efficient5; index++) 
+  {
+    CountZeros = CountZeros + parseInt((number/Math.pow(5,index)));
+  }
+  return CountZeros+1;
 }
+
