a1d10b2754f493f9688951ac381b4d53478ea4c6
panikutysh <pani.kutysh@gmail.com>
2018-09-28 15:28:44 +0300

    getZerosCount

--- src/index.js
+++ src/index.js
@@ -1,16 +1,73 @@
+var Dividers = [];
+var CountZerosDividers = [];
 module.exports = function getZerosCount(number) {
   var CountZeros = 0;
-  Efficient5 = 0; 
-  Divider = number;
-  while (Divider>=5) 
+  var CurrentCountZeros = 0;
+  if (FindDividersScaleOfNotation(10))
   {
-    Divider = Divider/5;
-    Efficient5 = Efficient5 + 1;
+    for (let index = 0; index < Dividers.length; index++) 
+    {
+      CurrentCountZeros = UseLegendresFormula(Dividers[index],number);
+      if ((CurrentCountZeros<CountZeros)&&(CountZeros>0))
+      {
+        CountZeros = CurrentCountZeros;
+      }
+      else if (CountZeros=0) 
+      {
+        CountZeros = CurrentCountZeros;
+      }
+    }
   }
-  for (let index = 1; index < Efficient5; index++) 
-  {
-    CountZeros = CountZeros + parseInt((number/Math.pow(5,index)));
-  }
-  return CountZeros+1;
+  return CurrentCountZeros;
 }
 
+function FindDividersScaleOfNotation(ScaleOfNotation) {
+  var Divider = 2;
+  var NotFound = true;
+  while (ScaleOfNotation > 1)
+  {
+    while (ScaleOfNotation % Divider == 0) 
+    {
+      ScaleOfNotation = ScaleOfNotation / Divider;
+      for (let index = 0; index < Dividers.length; index++) 
+      {
+        if (Dividers[index]==Divider)
+        {
+          NotFound = false;
+        }  
+      }
+      if (NotFound)
+      {
+        Dividers.push(Divider);
+      }
+      NotFound = true;
+    }
+    Divider = Divider + 1;
+  }
+  return true;  
+}
+
+function UseLegendresFormula(Divider,Factorial) 
+{
+  var power = 1;
+  var flag = true;
+  var CountZerosDividerInFactorial = 0;
+  while(flag)
+  {
+    if (Factorial >= Math.pow(Divider,power))
+    {
+      power = power + 1; 
+    }
+    else
+    {
+      flag = false;
+    }
+  }
+  while (power > 0 )
+  {
+    CountZerosDividerInFactorial = CountZerosDividerInFactorial + parseInt(Factorial/(Math.pow(Divider,power)));
+    power = power - 1;
+  }
+
+  return CountZerosDividerInFactorial;
+} 
